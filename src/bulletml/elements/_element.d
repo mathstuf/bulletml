09beb5c584091b742a852c8f20a4d02763a5596f
Ben Boeckel <mathstuf@gmail.com>
2014-03-27 03:34:46 -0400

    Improve the element base class

--- src/bulletml/elements/_element.d
+++ src/bulletml/elements/_element.d
@@ -2,7 +2,38 @@ module bulletml.elements._element;
 
 public import std.xml;
 
-public interface Element {
-  public void register(ref DocumentParser p);
-  public void parse(Element e);
+public class InvalidBulletML: Exception {
+  private:
+    public this(string msg) {
+      super(msg);
+    }
+}
+
+public class InvalidTag: InvalidBulletML {
+  private:
+    public this(string msg) {
+      super(msg);
+    }
+}
+
+public class InvalidAttribute: InvalidBulletML {
+  private:
+    public this(string msg) {
+      super(msg);
+    }
+}
+
+public class BulletMLElement {
+  private:
+    public void setup(ElementParser p);
+
+    protected void run(ElementParser p) {
+      p.onStartTag[null] = (ElementParser xml) {
+        throw new InvalidTag("The \'" ~ xml.tag().name ~ "\' " ~
+                             "is not a valid child of the " ~
+                             "\'" ~ p.tag().name ~ "\' tag.");
+      };
+
+      p.parse();
+    }
 }
