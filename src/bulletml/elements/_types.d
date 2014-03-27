af7293a6f30c829b97dfe3c6530ca99860902b34
Ben Boeckel <mathstuf@gmail.com>
2014-03-27 01:56:03 -0400

    Add enumeration types used in BulletML

---
+++ src/bulletml/elements/_types.d
@@ -0,0 +1,20 @@
+module bulletml.elements._types;
+
+public enum MotionType {
+  ABSOLUTE,
+  RELATIVE,
+  SEQUENCE
+}
+
+public enum Orientation {
+  NONE,
+  VERTICAL,
+  HORIZONTAL
+}
+
+public enum Direction {
+  AIM,
+  ABSOLUTE,
+  RELATIVE,
+  SEQUENCE
+}
