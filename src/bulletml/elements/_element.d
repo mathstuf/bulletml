7ddc2ba1aa0ee558dafc702e89df5b3ade877740
Ben Boeckel <mathstuf@gmail.com>
2014-03-27 03:36:21 -0400

    Add a way to distinguish elements

--- src/bulletml/elements/_element.d
+++ src/bulletml/elements/_element.d
@@ -25,13 +25,21 @@ public class InvalidAttribute: InvalidBulletML {
 
 public class BulletMLElement {
   private:
+    string name_;
+
     public void setup(ElementParser p);
 
+    public string name() {
+      return name_;
+    }
+
     protected void run(ElementParser p) {
+      name_ = p.tag().name;
+
       p.onStartTag[null] = (ElementParser xml) {
         throw new InvalidTag("The \'" ~ xml.tag().name ~ "\' " ~
                              "is not a valid child of the " ~
-                             "\'" ~ p.tag().name ~ "\' tag.");
+                             "\'" ~ name_ ~ "\' tag.");
       };
 
       p.parse();
