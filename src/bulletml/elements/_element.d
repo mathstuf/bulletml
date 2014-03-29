4614a95de512cb0698ebfc9c9c459fac6d9a6473
Ben Boeckel <mathstuf@gmail.com>
2014-03-29 00:39:44 -0400

    Add helpers to parse child elements

--- src/bulletml/elements/_element.d
+++ src/bulletml/elements/_element.d
@@ -1,5 +1,8 @@
 module bulletml.elements._element;
 
+private import std.traits;
+private import std.typecons;
+
 public import std.xml;
 
 public class InvalidBulletML: Exception {
@@ -70,5 +73,135 @@ public class BulletMLElement {
       };
 
       p.parse();
+    }
+}
+
+private void _parse(P, D)(ElementParser p, string tag, ref D store,
+                          ref bool parsed) {
+  p.onStartTag[tag] = (ElementParser xml) {
+    if (parsed) {
+      throw new DuplicateTag(xml, p);
+    }
+
+    D dat = new D;
+    P elem = new P(dat);
+
+    elem.setup(xml);
+
+    store = dat;
+    parsed = true;
+  };
+}
+
+private void _parse(P, D)(ElementParser p, string tag, ref D store,
+                          ref bool parsed, ref Fuse fuse) {
+  p.onStartTag[tag] = (ElementParser xml) {
+    if (parsed) {
+      throw new DuplicateTag(xml, p);
+    }
+
+    D dat = new D;
+    P elem = new P(dat);
+
+    elem.setup(xml);
+
+    if (fuse !is null) {
+      fuse.defuse();
+    }
+    store = dat;
+    parsed = true;
+  };
+}
+
+private void _parse(P, D)(ElementParser p, string tag, ref D store[]) {
+  p.onStartTag[tag] = (ElementParser xml) {
+    D dat = new D;
+    P elem = new P(dat);
+
+    elem.setup(xml);
+
+    store ~= dat;
+  };
+}
+
+private void _parse(P, D, T)(ElementParser p, string tag, ref D store[]) {
+  p.onStartTag[tag] = (ElementParser xml) {
+    T dat = new T;
+    P elem = new P(dat);
+
+    elem.setup(xml);
+
+    store ~= *new D(dat);
+  };
+}
+
+private void _parse(P, D, U : T*, T)(ElementParser p, string tag, ref D store[]) {
+  p.onStartTag[tag] = (ElementParser xml) {
+    T dat = new T;
+    P elem = new P(dat);
+
+    elem.setup(xml);
+
+    store ~= *new D(&dat);
+  };
+}
+
+public void parseOne(P, D)(ElementParser p, string tag, ref D store) {
+  bool parsed;
+  Fuse fuse = new Fuse(new MissingTag(tag, p));
+
+  _parse!(P, D)(p, tag, store, parsed, fuse);
+}
+
+public void parseOptional(P, D)(ElementParser p, string tag, ref Nullable!D store) {
+  bool parsed;
+
+  _parse!(P, D)(p, tag, store, parsed);
+}
+
+public void parseOneOf(P, D)(ElementParser p, string tags[], ref D store) {
+  bool parsed;
+  Fuse fuse = new Fuse(new MissingTag("<many>", p));
+
+  assert(D.AllowedTypes.length == tags.length);
+
+  for (int i = 0; i < tags.length; ++i) {
+    _parse!(P, store.AllowedTypes[i])(p, tags[i], store, parsed, fuse);
+  }
+}
+
+public void parseMany(P, D)(ElementParser p, string tags, ref D store[]) {
+  _parse!(P, D)(p, tags, store);
+}
+
+public void parseManyOf(P, D)(ElementParser p, string tags[], ref D store[]) {
+  static assert(P.AllowedTypes.length == D.AllowedTypes.length);
+  assert(D.AllowedTypes.length == tags.length);
+
+  foreach (i, T; D.AllowedTypes) {
+    static if (isPointer!T) {
+      _parse!(P.AllowedTypes[i], D, T, PointerTarget!T)(p, tags[i], store);
+    } else {
+      _parse!(P.AllowedTypes[i], D, T)(p, tags[i], store);
+    }
+  }
+}
+
+private class Fuse {
+  private:
+    Throwable exc;
+
+    public this(Throwable err) {
+      exc = err;
+    }
+
+    public ~this() {
+      if (exc !is null) {
+        throw exc;
+      }
+    }
+
+    public void defuse() {
+      exc = null;
     }
 }
