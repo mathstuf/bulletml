bdd0e882177b003fd655e181e6eceb1c512ad25e
Ben Boeckel <mathstuf@gmail.com>
2014-03-29 01:32:57 -0400

    Implement the parsing interface

--- src/bulletml/parser.d
+++ src/bulletml/parser.d
@@ -1,3 +1,27 @@
 module bulletml.parser;
 
-private import bulletml.elements._all;
+public import bulletml.data.bulletml;
+private import bulletml.elements.bulletml;
+
+private import std.stream;
+private import std.xml;
+
+public BulletML parse(string fname) {
+  return parse(new File(fname, FileMode.In));
+}
+
+public BulletML parse(InputStream istr) {
+  char[] contents;
+  while (!istr.eof) {
+    char[] chunk;
+    istr.read(chunk);
+    contents ~= chunk;
+  }
+  DocumentParser p = new DocumentParser(contents.idup);
+  BulletML bml = new BulletML;
+  EBulletML elem = new EBulletML(bml);
+
+  elem.setup(p);
+
+  return bml;
+}
