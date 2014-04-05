fb21ba6571aecfdc2ea6f7b9d86855fa5c32ba3c
Ben Boeckel <mathstuf@gmail.com>
2014-04-04 23:00:06 -0400

    Add a top-level module for application use

---
+++ src/bulletml/bulletml.d
@@ -0,0 +1,6 @@
+module bulletml.bulletml;
+
+public import bulletml.data.bulletml;
+public import bulletml.parser;
+public import bulletml.runner;
+public import bulletml.flatten: ResolvedBulletML, resolve;
