95e27a94090d4b0256b80ee5cb60264afae1fafd
Ben Boeckel <mathstuf@gmail.com>
2014-11-01 17:02:34 -0400

    test: test files individually

--- test/test_parse.d
+++ test/test_parse.d
@@ -1,20 +1,17 @@
-private import std.file;
 private import std.stdio;
 
 private import bml = bulletml.bulletml;
 
 int main(string[] args) {
-  string dir = args[1];
+  string fname = args[1];
   int ret = 0;
 
-  foreach (string fname; dirEntries(dir, "*.xml", SpanMode.breadth)) {
-    try {
-      bml.BulletML b = bml.parse(fname);
-      writeln("Successfully parsed " ~ fname);
-    } catch (Throwable t) {
-      writeln("Caught exception while parsing " ~ fname ~ ": " ~ t.toString());
-      ret = 1;
-    }
+  try {
+    bml.BulletML b = bml.parse(fname);
+    writeln("Successfully parsed " ~ fname);
+  } catch (Throwable t) {
+    writeln("Caught exception while parsing " ~ fname ~ ": " ~ t.toString());
+    ret = 1;
   }
 
   return ret;
